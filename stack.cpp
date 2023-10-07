b423802fa482f9199c0dcb8dab64df1312454a41
HRITU RAJ <112776533+hritu1701@users.noreply.github.com>
2023-10-07 23:01:39 +0530

    created stack

---
+++ stack.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main()
+{
+    stack<int>st;
+    st.push(1);
+    st.push(2);
+    st.push(5);
+    st.emplace(9);
+    cout<<st.top();
+
+}
